d297e49db9cf9b1d91207d7aafb017a6b6934702
Austin Potts <austinpotts@Andariels-Air.lan>
2023-08-01 09:56:47 -0400

    Configured Project - SwiftUI

---
+++ TrackMyExpenses/TrackMyExpenses/TrackMyExpensesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackMyExpensesApp.swift
+//  TrackMyExpenses
+//
+//  Created by Austin Potts on 8/1/23.
+//
+
+import SwiftUI
+
+@main
+struct TrackMyExpensesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
