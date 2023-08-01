0625b8b45e22023e317b8cedc883a143d3f83de5
Austin Potts <austinpotts@Andariels-Air.lan>
2023-08-01 19:55:10 -0400

    Configured Category Listing + Helper Extensions

---
+++ TrackMyExpenses/TrackMyExpenses/RecentTransactionList.swift
@@ -0,0 +1,66 @@
+//
+//  RecentTransactionList.swift
+//  TrackMyExpenses
+//
+//  Created by Austin Potts on 8/1/23.
+//
+
+import SwiftUI
+
+struct RecentTransactionList: View {
+    @EnvironmentObject var transactionListVM: TransactionListViewModel
+    
+    var body: some View {
+        VStack {
+            HStack {
+                //MARK: Header Title
+                Text("Recent Transactions")
+                    .bold()
+                
+                Spacer()
+                
+                //MARK: Header Link
+                NavigationLink {
+                    
+                } label:  {
+                    HStack(spacing: 4) {
+                        Text("See All")
+                        Image(systemName: "chevron.right")
+                    }
+                    .foregroundColor(Color.text)
+                }
+
+            }
+            .padding(.top)
+            
+            //MARK: Recent Transactions
+            ForEach(Array(transactionListVM.transaction.prefix(5).enumerated()),  id: \.element) { index, transaction in
+                TransactionRow(transaction: transaction)
+                
+                Divider()
+                    .opacity(index == 4 ? 0 : 1)
+            }
+        }
+        .padding()
+        .background(Color.systemBackground)
+        .clipShape(RoundedRectangle(cornerRadius: 20, style: .continuous))
+        .shadow(color: Color.primary.opacity(0.2), radius: 10, x: 0, y: 5)
+    }
+}
+
+struct RecentTransactionList_Previews: PreviewProvider {
+       static let transactionListVM: TransactionListViewModel = {
+        let transactionListVM = TransactionListViewModel()
+        transactionListVM.transaction = transactionListPreviewData
+        return transactionListVM
+    }()
+    static var previews: some View {
+        Group {
+            RecentTransactionList()
+            RecentTransactionList()
+                .preferredColorScheme(.dark)
+        }
+        .environmentObject(transactionListVM)
+
+    }
+}
